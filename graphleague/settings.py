d71c56fe327a0d043432e3967b8f52b9420b3e7f
Douglas T <dvtiepolo@gmail.com>
2024-03-03 19:10:36 +0000

    update settings

--- graphleague/settings.py
+++ graphleague/settings.py
@@ -21,12 +21,16 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-$i2!us7zmr52mn8anr!3aplt3^p#z&yv*938$4+rbzhyx=r9la'
+SECRET_KEY = os.environ.get('SECRET_KEY', default='django-insecure-$i2!us7zmr52mn8anr!3aplt3^p#z&yv*938$4+rbzhyx=r9la')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'RENDER' not in os.environ
 
-ALLOWED_HOSTS = ['graphleague.onrender.com']
+ALLOWED_HOSTS = []
+
+RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
+if RENDER_EXTERNAL_HOSTNAME:
+    ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
 
 
 # Application definition
