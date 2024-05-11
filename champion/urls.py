cbfe85dadd5d1fd0cbb3e956353337879d816dd6
Douglas T <dvtiepolo@gmail.com>
2024-05-11 17:01:40 +0100

    update assets

--- champion/urls.py
+++ champion/urls.py
@@ -1,7 +1,9 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import path
 from . import views
 
 urlpatterns = [
     path('', views.champions, name="champions"),
     path('<str:champion>/', views.champion_details, name="champion")
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
