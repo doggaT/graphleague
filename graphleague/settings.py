7e125637459ac69296075dc836d30d5fd6d998f3
Douglas T <dvtiepolo@gmail.com>
2024-05-11 16:54:15 +0100

    update assets

--- graphleague/settings.py
+++ graphleague/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY', default=os.getenv("SECRET_KEY"))
+SECRET_KEY = os.environ.get('SECRET_KEY', default='django-insecure-$i2!us7zmr52mn8anr!3aplt3^p#z&yv*938$4+rbzhyx=r9la')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'RENDER' not in os.environ
@@ -48,7 +48,6 @@ INSTALLED_APPS = [
     'champion',
     'match'
 ]
-
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
@@ -59,6 +58,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
 
 ROOT_URLCONF = 'graphleague.urls'
 
