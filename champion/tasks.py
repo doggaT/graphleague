b9d2a333dacb4eca38c2b769121db6c558372209
Douglas T <dvtiepolo@gmail.com>
2024-05-12 15:04:45 +0100

    fix unit test

--- champion/tasks.py
+++ champion/tasks.py
@@ -3,5 +3,11 @@ from champion.models import Champion
 
 
 @shared_task
-def run_champion_loader():
+def run_champion_loader(apps, schema_editor):
     Champion.champion_loader()
+
+    champion_model = apps.get_model("champion", "Champion")
+    champions = Champion.champion_loader()
+
+    for champion in champions:
+        champion_model.objects.update_or_create(**champion)
