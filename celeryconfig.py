0e852c9b274b496c7ef3afac7bb5b5d7499c38ef
Douglas T <dvtiepolo@gmail.com>
2024-05-12 15:02:21 +0100

    fix unit test

--- celeryconfig.py
+++ celeryconfig.py
@@ -2,14 +2,14 @@ import os
 from celery import Celery
 from celery.schedules import crontab
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'graphleague.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "graphleague.settings")
 
-app = Celery('champion')
+app = Celery("champion")
 app.conf.beat_schedule = {
-    'weekly-update': {
-        'task': 'champion.tasks.run_champion_loader',
-        'schedule': crontab(day_of_week="2", hour="10", minute="0"),
+    "weekly-update": {
+        "task": "champion.tasks.run_champion_loader",
+        "schedule": crontab(day_of_week="2", hour="10", minute="0"),
     },
 }
-app.config_from_object('django.conf:settings', namespace='CELERY')
+app.config_from_object("django.conf:settings", namespace="CELERY")
 app.autodiscover_tasks()
