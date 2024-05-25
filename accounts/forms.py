c3f081f29bfe35688e923a9fb88002f63e9efe3c
Douglas T <dvtiepolo@gmail.com>
2024-05-25 18:01:42 +0100

    add login/registration/logout

--- accounts/forms.py
+++ accounts/forms.py
@@ -5,43 +5,63 @@ from django.forms.widgets import PasswordInput, TextInput
 
 
 class CreateUserForm(UserCreationForm):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-
-        self.fields["username"].widget.attrs["class"] = "form-control"
-        self.fields["username"].widget.attrs["id"] = "floatingInput"
-        self.fields["username"].widget.attrs["placeholder"] = "username"
-        self.fields["username"].widget.attrs["type"] = "username"
-
-        self.fields["email"].widget.attrs["class"] = "form-control"
-        self.fields["email"].widget.attrs["id"] = "floatingEmail"
-        self.fields["email"].widget.attrs["placeholder"] = "name@example.com"
-
-        self.fields["password1"].widget.attrs["class"] = "form-control"
-        self.fields["password1"].widget.attrs["id"] = "floatingPassword"
-        self.fields["password1"].widget.attrs["placeholder"] = "Password"
-
-        self.fields["password2"].widget.attrs["class"] = "form-control"
-        self.fields["password2"].widget.attrs["id"] = "floatingConfirmPassword"
-        self.fields["password2"].widget.attrs["placeholder"] = "Confirm Password"
-
     class Meta:
         model = User
         fields = ["username", "email", "password1", "password2"]
 
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self.fields["username"].widget.attrs.update({
+            "class": "form-control",
+            "id": "floatingInput",
+            "placeholder": "Username",
+            "type": "text"
+        })
+
+        self.fields["email"].widget.attrs.update({
+            "class": "form-control",
+            "id": "floatingEmail",
+            "placeholder": "name@example.com",
+            "type": "email"
+        })
+
+        self.fields["password1"].widget.attrs.update({
+            "class": "form-control",
+            "id": "floatingPassword",
+            "placeholder": "Password",
+            "type": "password",
+            "autocomplete": "off"
+        })
+
+        self.fields["password2"].widget.attrs.update({
+            "class": "form-control",
+            "id": "floatingConfirmPassword",
+            "placeholder": "Confirm Password",
+            "type": "password",
+            "autocomplete": "off"
+        })
+
 
 class LoginUserForm(AuthenticationForm):
-    username = forms.CharField(widget=TextInput())
-    password = forms.CharField(widget=PasswordInput())
+    class Meta:
+        model = User
+        fields = ["username", "password"]
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
-        self.fields["username"].widget.attrs["class"] = "form-control"
-        self.fields["username"].widget.attrs["id"] = "floatingInput"
-        self.fields["username"].widget.attrs["placeholder"] = "username"
-        self.fields["username"].widget.attrs["type"] = "username"
+        self.fields["username"].widget.attrs.update({
+            "class": "form-control",
+            "id": "floatingInput",
+            "placeholder": "Username",
+            "type": "text"
+        })
 
-        self.fields["password"].widget.attrs["class"] = "form-control"
-        self.fields["password"].widget.attrs["id"] = "floatingPassword"
-        self.fields["password"].widget.attrs["placeholder"] = "Password"
+        self.fields["password"].widget.attrs.update({
+            "class": "form-control",
+            "id": "floatingPassword",
+            "placeholder": "Password",
+            "type": "password",
+            "autocomplete": "off"
+        })
