20109fd6e47c103fef1c592486dc4126e43ec7cd
Douglas T <dvtiepolo@gmail.com>
2024-06-13 20:54:17 +0100

    configure celery

--- celery_app.py
+++ celery_app.py
@@ -1,10 +1,11 @@
 from __future__ import absolute_import, unicode_literals
 import os
 from celery import Celery
+from celeryconfig import broker_url, result_backend
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "graphleague.settings")
 
-app = Celery("graphleague")
+app = Celery("graphleague", broker=broker_url, backend=result_backend)
 app.config_from_object("celeryconfig")
 app.autodiscover_tasks()
 
